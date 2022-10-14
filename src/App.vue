53a2e9834f9535d58026c952e04c1c048d13eb89
Aleksey Lisun <insuline.ru@gmail.com>
2022-10-14 22:43:06 +0300

    remove ts

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@ import data from './data.json'
 import { markers } from './txs';
 onMounted(() => {
 
+  // @ts-ignore
   const firstChart = createChart('first-chart', { layout: { textColor: 'black', background: { type: 'solid', color: 'white' } }, width: window.innerWidth, height: window.innerHeight, localization: { priceFormatter: (p: number) => p.toFixed(0) } });
 
   const areaSeries = firstChart.addAreaSeries({ lineColor: '#2962FF', topColor: '#2962FF', bottomColor: 'rgba(41, 98, 255, 0.28)' });
 
+  // @ts-ignore
   areaSeries.setData(data)
 
   firstChart.timeScale().applyOptions({
@@ -31,17 +33,3 @@ onMounted(() => {
     <div id="first-chart" style="height: 100vh; width: 100%;"></div>
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
